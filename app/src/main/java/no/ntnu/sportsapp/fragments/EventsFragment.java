55578fcefde5f9d3eb4670e5a5225c3edf1835f3
Sigurd <sigurd1994@hotmail.com>
2020-11-06 12:43:17 +0100

    EVENT DISPLAY DESIGN CHANGED
    Go here if we decide not to go for this design

--- app/src/main/java/no/ntnu/sportsapp/fragments/EventsFragment.java
+++ app/src/main/java/no/ntnu/sportsapp/fragments/EventsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,12 +14,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import no.ntnu.sportsapp.R;
 import no.ntnu.sportsapp.adapter.EventListAdapter;
 import no.ntnu.sportsapp.model.Event;
 import no.ntnu.sportsapp.rest.ApiClient;
 import no.ntnu.sportsapp.rest.AppInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class EventsFragment extends Fragment {
     private SwipeRefreshLayout swipeRefreshLayout;
@@ -51,7 +56,26 @@ public class EventsFragment extends Fragment {
     }
 
     public void setEventList() {
-    //api call to get events
+    Call<List<Event>> call = ApiClient
+            .getSingleton()
+            .getApi()
+            .getAllEvents();
+    call.enqueue(new Callback<List<Event>>() {
+        @Override
+        public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
+            if(response.isSuccessful()){
+                events = (ArrayList<Event>) response.body();
+                adapter.setEvents(events);
+            }else{
+                Toast.makeText(getContext(), "failed to fetch events, try again", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onFailure(Call<List<Event>> call, Throwable t) {
+
+        }
+    });
 
     }
 
