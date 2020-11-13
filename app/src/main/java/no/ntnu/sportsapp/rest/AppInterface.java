374953909f07300a26db6dfcb39b180fb543b922
Sigurd <sigurd1994@hotmail.com>
2020-11-13 11:39:53 +0100

    Updated retrofitcalls
    All calls implemented

--- app/src/main/java/no/ntnu/sportsapp/rest/AppInterface.java
+++ app/src/main/java/no/ntnu/sportsapp/rest/AppInterface.java
@@ -1,11 +1,15 @@
 package no.ntnu.sportsapp.rest;
 
+import java.sql.Time;
+import java.util.Date;
 import java.util.List;
 
 import no.ntnu.sportsapp.model.Event;
+import no.ntnu.sportsapp.model.User;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
@@ -17,19 +21,51 @@ import retrofit2.http.Query;
 
 public interface AppInterface {
 
-    //LOGIN AND REGISTRATION
+    //LOGIN, REGISTRATION AND CURRENTUSER\\
 
-
+    @FormUrlEncoded
     @GET("auth/login")
     public Call<ResponseBody> login(@Field("uid") String username,
                                     @Field("pwd") String password);
 
     @FormUrlEncoded
     @POST("auth/create")
-    public Call<ResponseBody> registerUser(@Field("uid") String username,
-                                           @Field("pwd") String password,
-                                           @Field("email") String email);
-    //EVENT SERVICES
+    public Call<ResponseBody> registerUser(@Field("fname") String firstName,
+                                           @Field("lname") String lastName,
+                                           @Field("email") String email,
+                                           @Field("pwd") String pwd);
+    @GET("auth/currentuser")
+    public Call<ResponseBody> currentUser();
+
+    //------------EVENT SERVICES------------\\
     @GET("event/allevents")
     public Call<List<Event>> getAllEvents();
+
+    @DELETE("event/remove")
+    public Call<ResponseBody> removeEvent(@Query("eventid") Long eventid);
+
+    @FormUrlEncoded
+    @POST("event/add")
+    public Call<ResponseBody> addEvent(@Field("sport") String sport,
+                                       @Field("description") String description,
+                                       @Field("date") Date date,
+                                       @Field("location") String location,
+                                       @Field("time") Time time,
+                                       @Field("maxPlayers") int maxPlayers);
+
+    @GET
+    public Call<List<User>> getAttenders(@Query("eventid") Long eventid);
+
+
+    //------------EVENT-INTERACTING------------\\
+
+    @PUT("event/joinevent")
+    public Call<ResponseBody> joinEvent(@Query("eventid") Long eventid);
+
+    @PUT("event/leave")
+    public Call<ResponseBody> leaveEvent(@Query("eventid") Long eventid);
+
+    @GET("event/myevents")
+    public Call<List<Event>> myEvents(@Query("userid") Long userid);
 }
+
