49769278f68bb69e6cea958f325aecbf90432615
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-11-16 13:15:52 +0100

    started eventactivity

--- app/src/main/java/no/ntnu/sportsapp/activity/EventActivity.java
+++ app/src/main/java/no/ntnu/sportsapp/activity/EventActivity.java
@@ -2,6 +2,8 @@ package no.ntnu.sportsapp.activity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,30 @@ import no.ntnu.sportsapp.R;
 
 public class EventActivity extends AppCompatActivity {
 
+    private TextView txtViewSport, txtViewDesc, txtViewDate, txtViewTime, txtViewLocation, txtViewMaxPlayers;
+    private Button attendBtn, notattendingBtn, generateTeamBtn;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_event);
+
+        txtViewSport = findViewById(R.id.eventsport);
+        txtViewDesc = findViewById(R.id.eventdesc);
+        txtViewDate = findViewById(R.id.eventdate);
+        txtViewTime = findViewById(R.id.eventtime);
+        txtViewMaxPlayers = findViewById(R.id.eventMaxPlayers);
+        txtViewLocation = findViewById(R.id.eventlocation);
+
+        //txtViewSport.setText(getIntent().getStringExtra("sport"));
+        //txtViewDesc.setText(getIntent().getStringExtra("description"));
+        //txtViewDate.setText(getIntent().getStringExtra("date"));
+        //txtViewTime.setText(getIntent().getStringExtra("time"));
+        //txtViewMaxPlayers.setText(getIntent().getStringExtra("maxPlayers"));
+        //txtViewLocation.setText(getIntent().getStringExtra("location"));
+
+        System.out.println("EVENT ACTIVITY OPENED");
+
+
     }
 }
